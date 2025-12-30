52f494d8203828cf5f0d6e685c3a81ec9da69225
ShiniShiho <59284549+shinishiho@users.noreply.github.com>
2025-12-30 23:11:53 +0700

    feat: cuda on nix

--- flake.nix
+++ flake.nix
@@ -13,33 +13,48 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        
         python = pkgs.python3;
         pythonEnv = python.withPackages (p: [
-          # Here goes all the libraries that can't be managed by uv because of dynamic linking issues
-          # or that you just want to be managed by nix for one reason or another
-          p.pandas
+          p.cupy
           p.google-re2
-          p.torchWithoutCuda
+          p.pandas
+          p.torchWithCuda
         ]);
       in
       {
         devShells.default =
           with pkgs;
           mkShell {
+            buildInputs = with pkgs; [
+              git gitRepo gnupg autoconf curl
+              procps gnumake util-linux m4 gperf unzip
+              cudatoolkit linuxPackages.nvidia_x11
+              libGLU libGL
+              xorg.libXi xorg.libXmu freeglut
+              xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
+              ncurses5 stdenv.cc binutils
+            ];
+
             packages = [
               uv
               python
               pythonEnv
-
               nodejs
-
               fish
             ];
 
             shellHook = ''
               export UV_PYTHON_PREFERENCE="only-system";
               export UV_PYTHON=${python}
+              export CUDA_PATH=${pkgs.cudatoolkit}
+              export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib
+              export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+              export EXTRA_CCFLAGS="-I/usr/include"
             '';
           };
       }
